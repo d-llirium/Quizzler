2d68cfedb2295a891ae2e1cf2436e4d013a1c45d
User <user@MB033361.local>
2024-08-12 18:33:30 -0300

    make question into a structure

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by User on 12/08/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        self.text = q
+        self.answer = a
+    }
+}
